fb1bf57604f7e10aaee07a2e5115ac3e89930566
Azhar <muhdazharudheen@gmail.com>
2021-11-12 18:31:36 +0400

    create collection view in homeviewcontroller

--- Classifieds/HomeViewController.swift
+++ Classifieds/HomeViewController.swift
@@ -9,8 +9,26 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    private lazy var collectionView = createCollectionView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+private extension HomeViewController {
+    func createCollectionView() -> UICollectionView {
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        cv.backgroundColor = .red
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
     }
 }
