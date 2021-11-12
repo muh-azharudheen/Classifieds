116e3ec63aebf0f52aba5effd00c36b27c09e97c
Azhar <muhdazharudheen@gmail.com>
2021-11-13 01:42:25 +0400

    create DetailViewController

---
+++ Classifieds/View/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  Classifieds
+//
+//  Created by Muhammed Azharudheen on 13/11/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classifieds/View/DetailViewController.m
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.m
+//  Classifieds
+//
+//  Created by Muhammed Azharudheen on 13/11/2021.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+@end
