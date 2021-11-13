6d03ca276d16eb3b4acab1a84cc6a8209df948a0
Azhar <muhdazharudheen@gmail.com>
2021-11-13 19:46:30 +0400

    implement service request

--- Classifieds/ServiceManager/ServiceManager.swift
+++ Classifieds/ServiceManager/ServiceManager.swift
@@ -14,6 +14,29 @@ class ServiceManager: APIServiceProtocol {
     static let shared = ServiceManager()
     
     func request<T: Decodable>(request: URLRequest, completion: @escaping (Result<T>) -> Void) {
-        
+        requestService(request: request, completion: completion)
+    }
+}
+
+private extension ServiceManager {
+    
+    private struct UnexpectedValuesError: Error { }
+    
+    func requestService<T: Decodable>(request: URLRequest, completion: @escaping (Result<T>) -> Void) {
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error: error))
+            } else if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                
+                if let decoded = try? JSONDecoder().decode(T.self, from: data) {
+                    completion(.success(decoded))
+                    return
+                }
+                completion(.failure(error: UnexpectedValuesError()))
+                
+            } else {
+                completion(.failure(error: UnexpectedValuesError()))
+            }
+        }.resume()
     }
 }
