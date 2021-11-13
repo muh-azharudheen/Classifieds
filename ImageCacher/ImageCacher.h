aa6b433cec09fc4d896371acc7b53884a9f64038
Azhar <muhdazharudheen@gmail.com>
2021-11-13 10:33:23 +0400

    create ios Framework ImageCacher

---
+++ ImageCacher/ImageCacher.h
@@ -0,0 +1,18 @@
+//
+//  ImageCacher.h
+//  ImageCacher
+//
+//  Created by Muhammed Azharudheen on 13/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageCacher.
+FOUNDATION_EXPORT double ImageCacherVersionNumber;
+
+//! Project version string for ImageCacher.
+FOUNDATION_EXPORT const unsigned char ImageCacherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageCacher/PublicHeader.h>
+
+
