19e064de6f96fabba11d3d4fb22502ae0836371f
Azhar <muhdazharudheen@gmail.com>
2021-11-13 20:48:43 +0400

    fix issues in service manager

--- Classifieds/ServiceManager/ServiceManager.swift
+++ Classifieds/ServiceManager/ServiceManager.swift
@@ -23,19 +23,33 @@ private extension ServiceManager {
     private struct UnexpectedValuesError: Error { }
     
     func requestService<T: Decodable>(request: URLRequest, completion: @escaping (Result<T>) -> Void) {
+        
+        let errorCompletion: (Error) -> Void = { error in
+            DispatchQueue.main.async {
+                completion(.failure(error: error))
+            }
+        }
+        
+        let successCompletion: (T) -> Void = { item in
+            DispatchQueue.main.async {
+                completion(.success(item))
+            }
+        }
+        
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
-                completion(.failure(error: error))
+                errorCompletion(error)
+                return
             } else if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
                 
-                if let decoded = try? JSONDecoder().decode(T.self, from: data) {
-                    completion(.success(decoded))
-                    return
+                do {
+                    let decoded = try JSONDecoder().decode(T.self, from: data)
+                    successCompletion(decoded)
+                } catch {
+                    errorCompletion(error)
                 }
-                completion(.failure(error: UnexpectedValuesError()))
-                
             } else {
-                completion(.failure(error: UnexpectedValuesError()))
+                errorCompletion(UnexpectedValuesError())
             }
         }.resume()
     }
