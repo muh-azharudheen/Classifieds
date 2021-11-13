d6ae74de6fc308c08f13fb0e23dbd12716dc942b
Azhar <muhdazharudheen@gmail.com>
2021-11-13 15:27:42 +0400

    add properties to Classified Object

--- Classifieds/Model/Classified.swift
+++ Classifieds/Model/Classified.swift
@@ -9,4 +9,19 @@ import Foundation
 
 class Classified {
     
+    var dateCreated: Date
+    var price: String
+    var name: String
+    var uuid: UUID
+    var imageURL: URL?
+    var thumbnailURL: URL?
+    
+    init(dateCreated: Date, price: String, name: String, uuid: UUID, imageURL: URL?, thumbnailURL: URL?) {
+        self.dateCreated = dateCreated
+        self.price = price
+        self.name = name
+        self.uuid = uuid
+        self.imageURL = imageURL
+        self.thumbnailURL = thumbnailURL
+    }
 }
