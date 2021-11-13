abd2072da7df56851c6b93f0d87633c16b9a6c24
Azhar <muhdazharudheen@gmail.com>
2021-11-13 19:28:45 +0400

    create Classifieds API Response Object

---
+++ Classifieds/APIResponses/ClassifiedsAPIResponse.swift
@@ -0,0 +1,17 @@
+//
+//  ClassifiedsAPIResponse.swift
+//  Classifieds
+//
+//  Created by Muhammed Azharudheen on 13/11/2021.
+//
+
+import Foundation
+
+struct ClassifiedsAPIResponse: Decodable {
+    let created_at: String
+    let price: String
+    let uid: String
+    let image_ids: [String]?
+    let image_urls: [String]?
+    let image_urls_thumbnails: [String]?
+}
