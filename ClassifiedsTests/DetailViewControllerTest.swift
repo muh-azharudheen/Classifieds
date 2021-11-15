9d9a04f07c249c6c555ce6738cc5e0d6f75b4b5c
Azhar <muhdazharudheen@gmail.com>
2021-11-15 22:07:28 +0400

    Add viewModel test for Detail ViewController

---
+++ ClassifiedsTests/DetailViewControllerTest.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewControllerTest.swift
+//  ClassifiedsTests
+//
+//  Created by Muhammed Azharudheen on 15/11/2021.
+//
+
+import XCTest
+@testable import Classifieds
+
+class DetailViewControllerTests: XCTestCase {
+    
+    func test_DetailViewControllerViewModel() {
+        
+        let title = "Test Title"
+        let subTitle = "Test Sub Title"
+        let price = "Test Price"
+        let viewModel = DetailViewModel(title: title, subTitle: subTitle, price: price)
+        
+        let sut = makeSUT(viewModel: viewModel)
+        _ = sut.view
+                
+        XCTAssertEqual(sut.labelTitle?.text, title)
+        XCTAssertEqual(sut.labelSubTitle?.text, subTitle)
+        XCTAssertEqual(sut.labelPrice?.text, price)
+    }
+    
+    private func makeSUT(viewModel: DetailViewModel) -> DetailViewController {
+        let controller = UIStoryboard(name: "Detail", bundle: nil).instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        controller.viewModel = viewModel
+        return controller
+    }
+    
+}
